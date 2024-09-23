85f8a2a91a240d16dee2870253b6a6d9ed19b73d
Kevin Yeh <kevinyeh415@proton.me>
2024-09-23 12:23:17 +0200

    slumpspel

--- 7070 slumptal/uppgift_1.py
+++ 7070 slumptal/uppgift_1.py
@@ -1,2 +1,13 @@
 import random 
-print (random.randrange(3))
+svar = input("Vill du spela? y/n")
+while svar == "y":
+    tal2 = random.randrange(1,7)
+    print(tal2)
+    tal1 = random.randrange(1,7)
+    print(tal1)
+    if tal2 == tal1:
+        svar = input("Vinst! Vill du spela igen? y/n")
+    elif tal2 != tal1:
+        svar = input("Förlust! Vill du spela igen? y/n")
+if svar == "n":
+        print("Inga spel för dig")
