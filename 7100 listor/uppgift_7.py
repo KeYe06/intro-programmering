e25b4057a068d7c2bfa1ea1e71ab646dde4fdc87
Kevin Yeh <kevinyeh415@proton.me>
2024-12-03 15:57:05 +0100

    Black Jack 2

--- 7100 listor/uppgift_7.py
+++ 7100 listor/uppgift_7.py
@@ -1,62 +1,116 @@
 import random
 #        0     1  2  3  4  5  6  7  8  9     10       11      12
 deck = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Queen", "King", "Jester"]
+deckx = ["Ace", "Ace", "Ace", "Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Queen", "King", "Jester" "Queen", "King", "Jester" "Queen", "King", "Jester" "Queen", "King", "Jester"]
 con = input("vill du spela black jack? j/n")
+card = 0
+cardcount = 49
 while con != "n":
     sdeck = []
     sdecksum = []
     adeck = []
     adecksum = []
+    adeckcheck = []
     for i in range (2):
-        sdeck.append(deck[random.randint(0, 12)])
+        card = random.randint(0,cardcount)
+        sdeck.append(deckx[card])
+        deckx.pop(card)
+        cardcount = cardcount - 1
     for i in range (2):
-        adeck.append(deck[random.randint(0, 12)])
+        card = random.randint(0,cardcount)
+        adeck.append(deckx[card]) 
+        deckx.pop(card)
+        cardcount = cardcount - 1
+    for i in adeck:
+        if i == "Ace":
+             adecksum.append(1)
+             adeckcheck.append(1)
+        elif i == "Queen" or i == "Jester" or i == "King":
+             adecksum.append(10)
+             adeckcheck.append(10)
+        else:
+             adecksum.append(i)
+             adeckcheck.append(i)
     print("Ditt deck:", sdeck)
     svar = input("HIT ELLER STAND")
     while svar != "stand":
-        sdeck.append(deck[random.randint(0,12)])
+        card = random.randint(0,cardcount)
+        sdeck.append(deckx[card]) 
+        deckx.pop(card)
+        cardcount = cardcount - 1
         print("Spelare: HIT")
-        print("Ditt deck:", sdeck)
         print("AI grumlar...")
-        if sum(adecksum) <= 16:
-            adeck.append(deck[random.randint(0,12)])
+        if sum(adeck) <= 16:
+            card = random.randint(0,cardcount)
+            adeck.append(deckx[card])
+            deckx.pop(card)
+            cardcount = cardcount - 1
+            for i in adeck:
+                if i in adeckcheck:
+                    pop.adeckcheck(i)
+                if i == "Ace":
+                    adecksum.append(1)
+                    adeckcheck.append(1)
+                elif i == "Queen" or i == "Jester" or i == "King":
+                    adecksum.append(10)
+                    adeckcheck.append(10)
+                else:
+                    adecksum.append(i)
+                    adeckcheck.append(i)
+            adeckscheck.extend(adecksum)
             print("AI: HIT")
         elif sum(adeck) >= 16:
             print("AI: STAND")
+        print("Ditt deck:", sdeck)
         svar = input("HIT ELLER STAND")
     while svar == "stand":
         print("AI grumlar...")
-        if sum(adecksum) <= 16:
-            adeck.append(deck[random.randint(0,12)])
+        if sum(adeck) <= 16:
+            card = random.randint(0,cardcount)
+            adeck.append(deckx[card]) 
+            deckx.pop(card)
+            cardcount = cardcount - 1
+            for i in adeck:
+                if i in adeckcheck:
+                    pop.adeckcheck(i)
+                if i == "Ace":
+                    adecksum.append(1)
+                elif i == "Queen" or i == "Jester" or i == "King":
+                    adecksum.append(10)
+                else:
+                    adecksum.append(i)
+            adeckscheck.extend(adecksum)
             print("AI: HIT")
             svar = input("HIT ELLER STAND")
         elif sum(adeck) >= 16:
             print("AI: STAND")
-            for burger in adeck:
-                if burger == "Ace":
-                    adecksum.append(1)
-                elif burger == "Queen" or burger == "king" or burger == "Jester":
-                    adecksum.append(10)
-                else:
-                    adecksum.append(burger)
-            for burger in sdeck:
-                if burger == "Ace":
+            for i in sdeck:
+                if i == "Ace":
                     sdecksum.append(1)
-                elif burger == "Queen" or burger == "king" or burger == "Jester":
+                elif i == "Queen" or i == "Jester" or i == "King":
                     sdecksum.append(10)
                 else:
-                    sdecksum.append(burger)
+                    sdecksum.append(i)
+            if sum(sdecksum) > 21 and "Ace" in sdeck:
+                sdecksum.pop[10]
+                sdecksum.append(1)
             print("Spelarens deck:", sdeck)
             print("Din summa:", sum(sdecksum))
-            print("AIs deck:", adeck)
-            print("AIs summa", sum(adecksum))
+            print("AIs summa", sum(adeck))
             if sum(sdecksum) == 21:
                 print("Black Jack för spelare")
-            elif sum(adecksum) == 21:
+            elif sum(adeck) == 21:
                 print("Black Jack för AI")
-            elif sum(sdecksum) < sum(adecksum) and sum(adecksum) <= 21:
+            elif sum(sdecksum) < sum(adeck) and sum(adeck) <= 21:
                 print("AI vinner")
-            elif sum(adecksum) < sum(sdecksum) and sum(sdecksum) <= 21:
+            elif sum(adeck) < sum(sdecksum) and sum(sdecksum) <= 21:
                 print("Spelare segrar")
+            elif sum(sdecksum) > 21 and sum(adeck) < 21:
+                print("AI vinner")
+            elif sum(adeck) > 21 and sum(sdecksum) < 21:
+                print("Spelare vinner")
+            else:
+                print("Lika")
+            con = input("Stick härifrån")
 
 
